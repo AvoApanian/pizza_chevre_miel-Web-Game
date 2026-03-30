5b4f7364a782a4284b8ea4bbe51545e5119078fd
AvoApanian <apanianavo3@gmail.com>
2026-03-30 05:25:15 +0400

    Add APK for download

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  publicDir: 'public',
+  base: '/pizza_chevre_miel-Web-Game/'
 })
